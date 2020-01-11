98891137da316d5108719d5d14e0ea515bfc8ff2
Szymon Kania <szymon.kania.dev@gmail.com>
2020-01-11 22:54:06 +0100

    List in one node

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -4,11 +4,16 @@ import CytoscapeComponent from 'react-cytoscapejs';
 import popper from 'cytoscape-popper';
 import klay from 'cytoscape-klay';
 import Cytoscape from 'cytoscape';
+import Temp from './Temp';
+import ReactDOM from 'react-dom';
 
 Cytoscape.use(klay);
 Cytoscape.use(popper);
 
 const Dashboard = props => {
+  const lists = props.lists ? props.lists.map((list, idx) => (
+    <Temp key={idx} list={list} previous={list.relations[0]}/>
+  )) : null;
   const elements = [
     { data: { id: 'one', label: 'Node 1' },  },
     { data: { id: 'two', label: 'Node 2' },  },
@@ -57,74 +62,72 @@ const Dashboard = props => {
 ];
  const [cy, setCy] = useState(null);
  async function cytoscapejsAfterInit (c){
-    console.log(c);
     await setCy(c);
-    console.log(cy);
     if(cy)
-    {
-      
-      
-      
-      let div  = document.createElement('div');  
-      let dash = document.getElementsByClassName('dashboard')[0];  
-      let text = document.createElement('div');    
-      text.innerHTML = 'Mój div';
-      text.classList.add('text')
-      div.appendChild( text );
+    {      
       cy.ready(function () {
-        let node = cy.nodes().first();
-        let popper = node.popper({
-          content: () => {             
-      
-            dash.appendChild( div );
-      
-            return div;
-          },
-          popper:{
-
-          }
-        });
-      
-        let update = () => {
-          console.log(cy.zoom())
-          text.style.transform= `scale(${cy.zoom()})`;
-          text.innerHTML = `${cy.zoom()}`;
-          text.style.color = "blue";
-          popper.scheduleUpdate();
-        };
-        node.on('position', update);
-        cy.on('pan resize zoom', update);
-
-        let nodeLast = null;
-        cy.on('grabon','node',function(e){
-          if(nodeLast)
-            nodeLast.removeClass("active");
-          let node = e.target;
-          node.addClass("active");         
-          nodeLast = node;
-        })
-        
-        
-      });
-      cy.on('click', function(e){     
-      
-       console.log(cy.nodes().positions(
-        function( node, i ){
-          console.log()
-          return {
-            x: node.position().x+10,
-            y: node.position().y+10
-          }
-        }
-       ))
-      })
+        initGraph(cy)
+      })  
     }     
  }
+
+ function initGraph(cy)
+ {
+  let div  = document.createElement('div');  
+  ReactDOM.render(lists, div);
+  let dash = document.getElementsByClassName('dashboard')[0];  
+  let text = document.createElement('div');    
+  text.innerHTML = 'Mój div';
+  text.classList.add('text')
+  div.appendChild( text );
+  cy.ready(function () {
+    let node = cy.nodes().first();
+    let popper = node.popper({
+      content: () => {             
+  
+        dash.appendChild( div );
+  
+        return div;
+      },
+      popper:{
+
+      }
+    });
+  
+    let update = () => {
+      console.log(cy.zoom())
+      text.style.transform= `scale(${cy.zoom()})`;
+      text.innerHTML = `${cy.zoom()}`;
+      text.style.color = "blue";
+      popper.scheduleUpdate();
+    };
+    node.on('position', update);
+    cy.on('pan resize zoom', update);
+
+    let nodeLast = null;
+    cy.on('grabon','node',function(e){
+      if(nodeLast)
+        nodeLast.removeClass("active");
+      let node = e.target;
+      node.addClass("active");         
+      nodeLast = node;
+    })
+    
+    
+  });
+ }
+
+
+
+
+
+
+
   return(
     <div className="dashboard">
       <h2>Dashboard</h2>
       <CytoscapeComponent  cy={(cy)=> {cytoscapejsAfterInit(cy)}} elements={elements} stylesheet={stylesheet} layout={{name: 'klay'}} style={ { width: '100%', height: '100%' } } />
-
+    
     </div>
   );
 }
